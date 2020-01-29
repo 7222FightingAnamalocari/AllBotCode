df6c1e39a77b3bec157d918da50825cf8aadf9e0
Solo Silver <solomonwagner@protonmail.com>
2020-01-29 14:50:48 -0500

    Revert to initial for game bot code
    Signed-off-by: Solo Silver <SolomonWagner@protonmail.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,171 +7,100 @@
 
 package frc.robot;
 
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
-
-
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Servo;
-
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
+ * methods corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+public class Robot extends TimedRobot
+{
+    private static final String DEFAULT_AUTO = "Default";
+    private static final String CUSTOM_AUTO = "My Auto";
+    private String autoSelected;
+    private final SendableChooser<String> chooser = new SendableChooser<>();
 
-  int joystick_ = 0;
-  Joystick joystick = new Joystick(joystick_);
-  Spark m_RF = new Spark(3);
-  Spark m_LF = new Spark(0);
-  Spark m_RR = new Spark(2);
-  Spark m_LR = new Spark(1);
-  Spark m_test = new Spark(4);
-
-  Servo mServo = new Servo(5);
-
-  SpeedControllerGroup right = new SpeedControllerGroup(m_RF, m_RR);
-  SpeedControllerGroup left = new SpeedControllerGroup(m_LF, m_LR);
-
-  DifferentialDrive drive = new DifferentialDrive(left, right);
-
-
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-    new Thread(() -> {
-      UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
-      camera.setResolution(320, 240);
-
-      CvSink cvSink = CameraServer.getInstance().getVideo();
-      CvSource outputStream = CameraServer.getInstance().putVideo("cam0", 320, 240);
-
-      Mat source = new Mat();
-      Mat output = new Mat();
-      while (!Thread.interrupted()) {
-        cvSink.grabFrame(source);
-        Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-        outputStream.putFrame(output);
-      }
-    });
-       right.setInverted(true);
-
-  }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-       switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-
-        left.set(-.5);
-        right.set(-.5);
-        wait(5000);
-        left.set(0);
-        right.set(0);
-        wait(5000);
-      
-
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    /**
+     * This method is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit()
+    {
+        chooser.setDefaultOption("Default Auto", DEFAULT_AUTO);
+        chooser.addOption("My Auto", CUSTOM_AUTO);
+        SmartDashboard.putData("Auto choices", chooser);
     }
-  }
 
+    /**
+     * This method is called every robot packet, no matter the mode. Use
+     * this for items like diagnostics that you want ran during disabled,
+     * autonomous, teleoperated and test.
+     * <p>
+     * This runs after the mode specific periodic methods, but before
+     * LiveWindow and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic()
+    {
+    }
 
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to
+     * the switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
+     */
+    @Override
+    public void autonomousInit()
+    {
+        autoSelected = chooser.getSelected();
+        // autoSelected = SmartDashboard.getString("Auto Selector",
+        // defaultAuto);
+        System.out.println("Auto selected: " + autoSelected);
+    }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-     
-   drive.arcadeDrive(joystick.getRawAxis(0), joystick.getRawAxis(1));
-   while(joystick.getRawButtonPressed(5)){
-  m_test.set(1);
-  }
-  if(joystick.getRawButtonReleased(5)) {
-   m_test.set(0);
-  }
-  while(joystick.getRawButtonPressed(3)){
-    m_test.set(-1);
-  }if(joystick.getRawButtonReleased(3)){
-    m_test.set(0);
-  }
-  while(joystick.getRawButtonPressed(6)){
-    mServo.set(0.5);
-  }
-  while(joystick.getRawButtonPressed(4)){
-    mServo.set(0.1);
-  }
-  
+    /**
+     * This method is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic()
+    {
+        switch (autoSelected)
+        {
+            case CUSTOM_AUTO:
+                // Put custom auto code here
+                break;
+            case DEFAULT_AUTO:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
 
-}
-  
+    /**
+     * This method is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic()
+    {
+    }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    /**
+     * This method is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic()
+    {
+    }
 }
